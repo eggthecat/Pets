fc4fc928536c939c28712f8c83d7aeef315f14d4
Dominic Montelongo and Heather Zurek <dominic@>
2019-03-26 13:01:46 -0700

    add javascript functions and buttons to change the page color

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+  $("button#light").click(function() {
+    $("body").removeClass();
+    $("body").addClass("light-background");
+  });
+  $("button#dark").click(function() {
+    $("body").removeClass();
+    $("body").addClass("dark-background");
+  });
+  $("button#original").click(function() {
+    $("body").removeClass();
+    $("body").addClass("original-background");
+  });
+});
